494111ef23cde29ce0d4a89cd786891f35599bab
georgeuwabor <63528996+georgeuwabor@users.noreply.github.com>
2023-01-18 14:34:13 -0500

    fable product

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import Collection from "./Pages/Collection";
+import Product from "./Pages/Product";
 
 function App() {
-  return <>{/* <Collection /> */}</>;
+  return (
+    <>
+      {/* <Collection /> */}
+      <Product />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Pages/Product.js
@@ -0,0 +1,13 @@
+import Navbar from "../Component/Navbar";
+import Fable from "../Component/Fable";
+
+const Product = () => {
+  return (
+    <>
+      <Navbar />
+      <Fable />
+    </>
+  );
+};
+
+export default Product;
