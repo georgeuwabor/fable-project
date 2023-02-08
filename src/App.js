fc549f46cc8047ee8fedf7f876e112fdeb2bdd71
georgeuwabor <63528996+georgeuwabor@users.noreply.github.com>
2023-02-07 23:24:00 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import Collection from "./Pages/Collection";
 import Product from "./Pages/Product";
 import Order from "./Pages/Order";
 import Profile from "./Pages/Profile";
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Count from "./Pages/Count";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Collection />}/>
-      <Route path="/collections" element={<Collection />}/>
-      <Route path="/products/:id" element={<Product />} />
-      <Route path="/orders" element={<Order />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/count" element={<Count />} />
-
+        <Route path="/" element={<Collection />} />
+        <Route path="/collections" element={<Collection />} />
+        <Route path="/products/:id" element={<Product />} />
+        <Route path="/orders" element={<Order />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/count" element={<Count />} />
       </Routes>
     </BrowserRouter>
   );
